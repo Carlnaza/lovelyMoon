1a49a0a070aade22c9823fbc5d70bad35cb16436
Carlo Nazareno <csnazareno94@gmail.com>
2020-12-24 12:53:22 -0800

    test server bug fix

--- server.js
+++ server.js
@@ -1,21 +1,26 @@
-require('dotenv').config()
-const { join } = require('path')
-const express = require('express')
+require('dotenv').config();
+const { join, resolve } = require('path');
+const express = require('express');
 
-const app = express()
+const app = express();
 
-app.use(express.static(join(__dirname, 'client', 'build')))
-app.use(express.urlencoded({ extended: true }))
-app.use(express.json())
+app.use(express.static(join(__dirname, 'client', 'build')));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-app.use(require('./controllers'))
+app.use(require('./controllers'));
 
-app.get('/*', (req, res) => {
-  res.sendFile(join(__dirname, 'client', 'build', 'index.html'))
-})
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(resolve(__dirname, 'client', 'build', 'index.html'))
+  })
+
+};
 
 require('./config')
   .then(() => app.listen(process.env.PORT || 3001), () => {
     console.log("App now listening on localhost:3000// :)")
   })
-  .catch(e => console.error(e))
+  .catch(e => console.error(e));
