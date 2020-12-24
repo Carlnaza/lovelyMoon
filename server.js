189c5083e00eb1ce16e31a65836e03e3fa0deb79
Carlo Nazareno <csnazareno94@gmail.com>
2020-12-24 13:29:23 -0800

    connection update

--- server.js
+++ server.js
@@ -1,26 +1,23 @@
-require('dotenv').config();
-const { join, resolve } = require('path');
-const express = require('express');
+const express = require('express')
+const { join } = require('path')
 
-const app = express();
-
-app.use(express.static(join(__dirname, 'client', 'build')));
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-
-app.use(require('./controllers'));
+const app = express()
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
+  app.use(express.static(join(__dirname, 'client', 'build')))
+}
 
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+app.use(require('./controllers'))
+
+if (process.env.NODE_ENV === 'production') {
   app.get('/*', (req, res) => {
-    res.sendFile(resolve(__dirname, 'client', 'build', 'index.html'))
+    res.sendFile(join(__dirname, 'client', 'build', 'index.html'))
   })
-
-};
+}
 
 require('./config')
-  .then(() => app.listen(process.env.PORT || 3001), () => {
-    console.log(`App now listening on localhost:${process.env.PORT}// :)`)
-  })
-  .catch(e => console.error(e));
+  .then(() => app.listen(process.env.PORT || 3001))
+  .catch(err => console.log(err))
